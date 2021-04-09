b6175d41624934ee89518bb677c30a33a1728d2e
Ítalo Andrade <italodeandra@gmail.com>
2021-04-10 00:03:36 +0200

    better debug discord error

--- src/pages/api/logDrain.ts
+++ src/pages/api/logDrain.ts
@@ -1,7 +1,7 @@
 import { cors, internalServerError, runMiddleware } from "next-library"
 import { NextApiRequest, NextApiResponse } from "next"
 import Log from "../../vercel/models/log"
-import axios from "axios"
+import axios, { AxiosError } from "axios"
 
 const integration = async (req: NextApiRequest, res: NextApiResponse) => {
   await runMiddleware(req, res, cors())
@@ -11,10 +11,13 @@ const integration = async (req: NextApiRequest, res: NextApiResponse) => {
   if (logs && logs.length) {
     try {
       await axios.post("https://discord.com/api/webhooks/830165634360016917/B4AgSgzn0fFgfLXOzNFtUsJrQkTDp3xaeVsCA6vkrtDx3fwjQ_SfznSl35fsbKqXhaMN", {
-        content: `\`\`\`json\n${JSON.stringify(logs)}\n\`\`\``
+        content: `\`\`\`json\n${JSON.stringify(logs)}\n\`\`\``.substr(0, 1999)
       })
     } catch (e) {
-      console.error(e)
+      const err: AxiosError = e
+      if (err.isAxiosError && err.response.data) {
+        console.error(err.response.data)
+      }
       return internalServerError(res)
     }
   }
