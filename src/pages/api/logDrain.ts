ff2553f25fd46289623ef64899e53bdb88e4fbc1
Ítalo Andrade <italodeandra@gmail.com>
2021-04-10 00:11:53 +0200

    filter logs to only errors

--- src/pages/api/logDrain.ts
+++ src/pages/api/logDrain.ts
@@ -6,9 +6,10 @@ import axios, { AxiosError } from "axios"
 const integration = async (req: NextApiRequest, res: NextApiResponse) => {
   await runMiddleware(req, res, cors())
 
-  const logs = req.body as Log[]
+  let logs = req.body || [] as Log[]
 
-  if (logs && logs.length) {
+  logs = logs.filter(l => l.statusCode?.toString().startsWith("5"))
+  if (logs.length) {
     try {
       await axios.post("https://discord.com/api/webhooks/830165634360016917/B4AgSgzn0fFgfLXOzNFtUsJrQkTDp3xaeVsCA6vkrtDx3fwjQ_SfznSl35fsbKqXhaMN", {
         content: `\`\`\`json\n${JSON.stringify(logs)}\n\`\`\``.substr(0, 1999)
