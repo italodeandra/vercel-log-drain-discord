361673a3612f9731318977c15d6fd98c863f8683
Ítalo Andrade <italodeandra@gmail.com>
2021-04-10 00:30:11 +0200

    format json

--- src/pages/api/logDrain.ts
+++ src/pages/api/logDrain.ts
@@ -12,7 +12,7 @@ const integration = async (req: NextApiRequest, res: NextApiResponse) => {
   if (logs.length) {
     try {
       await axios.post("https://discord.com/api/webhooks/830165634360016917/B4AgSgzn0fFgfLXOzNFtUsJrQkTDp3xaeVsCA6vkrtDx3fwjQ_SfznSl35fsbKqXhaMN", {
-        content: `\`\`\`json\n${JSON.stringify(logs)}\n\`\`\``.substr(0, 1999)
+        content: `\`\`\`json\n${JSON.stringify(logs, null, 2)}\n\`\`\``.substr(0, 1999)
       })
     } catch (e) {
       const err: AxiosError = e
