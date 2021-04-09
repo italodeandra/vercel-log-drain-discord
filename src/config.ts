c312acfb1bdc96b2c145776133998c7b4992ea2e
Ítalo Andrade <italodeandra@gmail.com>
2021-04-09 23:15:11 +0200

    rename env var

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   vercelApi: process.env.VERCEL_API || "https://api.vercel.com",
-  clientId: process.env.VERCEL_INTEGRATION_CLIENT_ID,
-  clientSecret: process.env.VERCEL_INTEGRATION_SECRET,
+  clientId: process.env.NEXT_PRIVATE_VERCEL_INTEGRATION_CLIENT_ID,
+  clientSecret: process.env.NEXT_PRIVATE_VERCEL_INTEGRATION_SECRET,
   baseUrl: process.env.BASE_URL || "http://localhost:3000",
 }
 
