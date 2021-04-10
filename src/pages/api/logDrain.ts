2aeab1b21afb0e8790c7e1ce570da563b3b3bcd4
Ítalo Andrade <italodeandra@gmail.com>
2021-04-10 14:21:55 +0200

    always log the entire log on file but if there's a message put it on the discord message content for easy reference

--- src/pages/api/logDrain.ts
+++ src/pages/api/logDrain.ts
@@ -4,7 +4,7 @@ import Log from "../../vercel/models/log"
 import axios, { AxiosError } from "axios"
 import FormData from "form-data"
 
-const wrapCode = (str = "") => `\`\`\`\n${str}\n\`\`\``
+const wrapCode = (str = "") => str/*`\`\`\`\n${str}\n\`\`\``*/
 
 const integration = async (req: NextApiRequest, res: NextApiResponse) => {
   await runMiddleware(req, res, cors())
@@ -14,36 +14,33 @@ const integration = async (req: NextApiRequest, res: NextApiResponse) => {
   logs = logs.filter((l) => l.statusCode?.toString().startsWith("5"))
   for (let log of logs) {
     try {
-      const content = log.message || JSON.stringify(log, null, 2)
-      if (content.length < 2000 - wrapCode().length) {
-        await axios.post(
-          "https://discord.com/api/webhooks/830165634360016917/B4AgSgzn0fFgfLXOzNFtUsJrQkTDp3xaeVsCA6vkrtDx3fwjQ_SfznSl35fsbKqXhaMN",
-          {
-            content: wrapCode(content),
-          }
-        )
-      } else {
-        let formData = new FormData()
-        formData.append("file", content, {
-          contentType: "text/plain",
-          knownLength: content.length,
-          filename: "big-log.txt",
-        })
-        await axios.post(
-          "https://discord.com/api/webhooks/830165634360016917/B4AgSgzn0fFgfLXOzNFtUsJrQkTDp3xaeVsCA6vkrtDx3fwjQ_SfznSl35fsbKqXhaMN",
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-              ...formData.getHeaders(),
-            },
-          }
-        )
+      const json = JSON.stringify(log, null, 2)
+      const content = log.message
+      let formData = new FormData()
+      formData.append("file", json, {
+        contentType: "application/json",
+        knownLength: json.length,
+        filename: "log.json",
+      })
+      if (content && content.length < 2000 - wrapCode().length) {
+        formData.append("content", wrapCode(content))
       }
+      await axios.post(
+        "https://discord.com/api/webhooks/830165634360016917/B4AgSgzn0fFgfLXOzNFtUsJrQkTDp3xaeVsCA6vkrtDx3fwjQ_SfznSl35fsbKqXhaMN",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+            ...formData.getHeaders(),
+          },
+        }
+      )
     } catch (e) {
       const err: AxiosError = e
       if (err.isAxiosError && err.response.data) {
         console.error(err.response.data)
+      } else {
+        console.error(err.message)
       }
       return internalServerError(res)
     }
