8b5bea3c7f47e0ac5c62a4bc0ed5b869b89b59f5
Ítalo Andrade <italodeandra@gmail.com>
2021-04-10 14:03:59 +0200

    send log json inside a code when it fits or inside a file if too big

--- src/pages/api/logDrain.ts
+++ src/pages/api/logDrain.ts
@@ -2,18 +2,42 @@ import { cors, internalServerError, runMiddleware } from "next-library"
 import { NextApiRequest, NextApiResponse } from "next"
 import Log from "../../vercel/models/log"
 import axios, { AxiosError } from "axios"
+import FormData from "form-data"
 
 const integration = async (req: NextApiRequest, res: NextApiResponse) => {
   await runMiddleware(req, res, cors())
 
-  let logs = req.body || [] as Log[]
+  let logs = req.body || ([] as Log[])
 
-  logs = logs.filter(l => l.statusCode?.toString().startsWith("5"))
-  if (logs.length) {
+  logs = logs.filter((l) => l.statusCode?.toString().startsWith("5"))
+  for (let log of logs) {
     try {
-      await axios.post("https://discord.com/api/webhooks/830165634360016917/B4AgSgzn0fFgfLXOzNFtUsJrQkTDp3xaeVsCA6vkrtDx3fwjQ_SfznSl35fsbKqXhaMN", {
-        content: `\`\`\`json\n${JSON.stringify(logs, null, 2)}\n\`\`\``.substr(0, 1999)
-      })
+      const json = JSON.stringify(log, null, 2)
+      if (json.length < 1980) {
+        await axios.post(
+          "https://discord.com/api/webhooks/830165634360016917/B4AgSgzn0fFgfLXOzNFtUsJrQkTDp3xaeVsCA6vkrtDx3fwjQ_SfznSl35fsbKqXhaMN",
+          {
+            content: `\`\`\`json\n${json}\n\`\`\``.substr(0, 1999),
+          }
+        )
+      } else {
+        let formData = new FormData()
+        formData.append("file", json, {
+          contentType: "application/json",
+          knownLength: json.length,
+          filename: "big-log.json",
+        })
+        await axios.post(
+          "https://discord.com/api/webhooks/830165634360016917/B4AgSgzn0fFgfLXOzNFtUsJrQkTDp3xaeVsCA6vkrtDx3fwjQ_SfznSl35fsbKqXhaMN",
+          formData,
+          {
+            headers: {
+              "Content-Type": "multipart/form-data",
+              ...formData.getHeaders(),
+            },
+          }
+        )
+      }
     } catch (e) {
       const err: AxiosError = e
       if (err.isAxiosError && err.response.data) {
