2095c0c92c72766eae0c3d6e487a22d00ba61fe8
Ítalo Andrade <italodeandra@gmail.com>
2021-04-10 20:11:20 +0200

    remove unused packages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,17 @@
 import Head from "next/head"
-import { ThemeProvider } from "@material-ui/core/styles"
-import { CacheProvider } from "@emotion/react"
-import CssBaseline from "@material-ui/core/CssBaseline"
-import createCache from "@emotion/cache"
-import theme from "../theme"
 import { VFC } from "react"
-
-export const cache = createCache({ key: "css", prepend: true })
 
 const MyApp: VFC<any> = ({ Component, pageProps }) => {
   return (
-    <CacheProvider value={cache}>
+    <>
       <Head>
         <title>Vercel Log Drain Discord</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
         <link rel="icon" href={"/favicon.ico"} />
       </Head>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
 
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </CacheProvider>
+      <Component {...pageProps} />
+    </>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
-import { NoSsr } from "@material-ui/core"
-import { FullBoxCenter } from "../components"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
 
 const Home = () => {
+  const router = useRouter()
+  useEffect(() => {
+    void router.push("https://vercel.com/integrations/log-drain-discord")
+  })
   return (
-    <NoSsr>
-      <FullBoxCenter>
-        Hi
-      </FullBoxCenter>
-    </NoSsr>
+    <div style={{ padding: 8 }}>
+      Redirecting to{" "}
+      <a href="https://vercel.com/integrations/log-drain-discord">
+        https://vercel.com/integrations/log-drain-discord
+      </a>
+    </div>
   )
 }
 
